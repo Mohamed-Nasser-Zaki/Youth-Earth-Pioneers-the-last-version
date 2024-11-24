0c3862e5bae7ba282a309fa4ca1debc85654c819
Mohamed-Nasser-Zaki <mohamednasserzaky77@gmail.com>
2024-11-24 20:43:55 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ProjectsPage from './pages/ProjectsPage';
+import MentorshipPage from './pages/MentorshipPage';
+import ScrollToTop from './components/ScrollToTop';
+
+const App = () => {
+  return (
+    <Router>
+      <ScrollToTop />
+      <div className="min-h-screen bg-white flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/mentorship" element={<MentorshipPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
